afb8bea4d5036658622b462cea557bf11c99960a
SalikhV <svalishev@jmart.kz>
2024-05-18 15:52:11 +0500

    unic images

--- lib/widgets/quote_item.dart
+++ lib/widgets/quote_item.dart
@@ -9,38 +9,49 @@ class QuoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imageUrl = 'https://source.unsplash.com/random/800x600?sig=${quote
+        .id}'; // Генерация уникального изображения
+
     return Padding(
       padding: EdgeInsets.all(10),
       child: Card(
         elevation: 5,
-        child: Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CachedNetworkImage(
-                imageUrl: 'https://source.unsplash.com/random/800x600',
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CachedNetworkImage(
+              imageUrl: imageUrl,
+              placeholder: (context, url) =>
+                  Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 200,
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    quote.quote,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '- ${quote.author}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 10),
-              Text(
-                quote.quote,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                '- ${quote.author}',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
