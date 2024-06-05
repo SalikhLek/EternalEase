b0535a19af53983f7c4b0105fc82f04278814609
SalikhV <svalishev@jmart.kz>
2024-06-05 14:23:57 +0500

    affirmation play and pause

--- lib/screens/affirmations_screen.dart
+++ lib/screens/affirmations_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter_sound/public/flutter_sound_player.dart';
 
-class AffirmationsScreen extends StatelessWidget {
+class AffirmationsScreen extends StatefulWidget {
+  @override
+  _AffirmationsScreenState createState() => _AffirmationsScreenState();
+}
+
+class _AffirmationsScreenState extends State<AffirmationsScreen> {
   final List<Map<String, String>> affirmations = [
     {
       'title': 'Аффирмация 1',
@@ -14,6 +20,69 @@ class AffirmationsScreen extends StatelessWidget {
     // Добавьте больше аффирмаций здесь
   ];
 
+  FlutterSoundPlayer _audioPlayer = FlutterSoundPlayer();
+  bool _isPlaying = false;
+  bool _isPaused = false;
+  double _playPosition = 0.0;
+  String? _currentPath;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer.openPlayer();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.closePlayer();
+    super.dispose();
+  }
+
+  Future<void> _togglePlayback(String audioPath) async {
+    if (_isPlaying && _currentPath == audioPath) {
+      await _audioPlayer.pausePlayer();
+      setState(() {
+        _isPaused = true;
+        _isPlaying = false;
+      });
+    } else if (_isPaused && _currentPath == audioPath) {
+      await _audioPlayer.resumePlayer();
+      setState(() {
+        _isPaused = false;
+        _isPlaying = true;
+      });
+    } else {
+      await _audioPlayer.stopPlayer();
+      setState(() {
+        _playPosition = 0.0;
+      });
+      _currentPath = audioPath;
+      await _audioPlayer.startPlayer(
+        fromURI: audioPath,
+        codec: Codec.mp3,
+        whenFinished: () {
+          setState(() {
+            _isPlaying = false;
+            _isPaused = false;
+            _playPosition = 0.0;
+          });
+        },
+      );
+      _audioPlayer.onProgress!.listen((event) {
+        if (event != null && event.duration != null) {
+          setState(() {
+            _playPosition = event.position.inMilliseconds /
+                event.duration!.inMilliseconds;
+          });
+        }
+      });
+      setState(() {
+        _isPlaying = true;
+        _isPaused = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,15 +92,24 @@ class AffirmationsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: affirmations.length,
         itemBuilder: (context, index) {
+          final title = affirmations[index]['title']!;
+          final audioPath = affirmations[index]['path']!;
           return Card(
             margin: EdgeInsets.all(10),
             child: ListTile(
-              title: Text(affirmations[index]['title']!),
-              onTap: () async {
-                final audioPath = affirmations[index]['path']!;
-                AudioPlayer audioPlayer = AudioPlayer();
-                await audioPlayer.play(audioPath, isLocal: true); // Указываем, что аудиофайл локальный
-              },
+              leading: Icon(Icons.music_note),
+              title: Text(title),
+              trailing: IconButton(
+                icon: Icon(_isPlaying && _currentPath == audioPath
+                    ? Icons.pause
+                    : Icons.play_arrow),
+                onPressed: () => _togglePlayback(audioPath),
+              ),
+              subtitle: _isPlaying && _currentPath == audioPath
+                  ? LinearProgressIndicator(
+                value: _playPosition,
+              )
+                  : null,
             ),
           );
         },
